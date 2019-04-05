6d86f9f714f49f0e6f60bf395781cd88ab25f4eb
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-04-05 15:58:19 +0200

    Make puppet-lint happy

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,25 +7,23 @@
 class ssh {
 
   $package = $::osfamily ? {
-    "Archlinux" => "openssh",
-    default     => "openssh-server",
+    'Archlinux' => 'openssh',
+    default     => 'openssh-server',
   }
   $service_name =  $::osfamily ? {
-    "Archlinux" => 'sshd.service',  # systemd workaround
-    "RedHat"    => 'sshd',
+    'Archlinux' => 'sshd.service',  # systemd workaround
+    'RedHat'    => 'sshd',
     default     => 'ssh',
   }
 
   package { $package: ensure => installed }
-  ~>
-  file { '/etc/ssh/sshd_config':
+  ~> file { '/etc/ssh/sshd_config':
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template('ssh/sshd.conf.erb')
+    content => template('ssh/sshd.conf.erb'),
   }
-  ~>
-  service { 'ssh':
+  ~> service { 'ssh':
     ensure     => running,
     name       => $service_name,
     hasstatus  => true,
@@ -35,7 +33,7 @@ class ssh {
 
   # Generate RSA keys reliably
   $rsa_priv = ssh_keygen({name => "ssh_host_rsa_${::fqdn}", dir => 'ssh/hostkeys'})
-  $rsa_pub  = ssh_keygen({name => "ssh_host_rsa_${::fqdn}", dir => 'ssh/hostkeys', public => 'true'})
+  $rsa_pub  = ssh_keygen({name => "ssh_host_rsa_${::fqdn}", dir => 'ssh/hostkeys', public => true})
 
   file { '/etc/ssh/ssh_host_rsa_key':
     owner   => 'root',
@@ -49,13 +47,13 @@ class ssh {
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => "ssh-rsa $rsa_priv host_rsa_${::hostname}\n",
+    content => "ssh-rsa ${rsa_priv} host_rsa_${::hostname}\n",
     notify  => Service['ssh'],
   }
 
   # Generate ECDSA keys reliably
   $ecdsa_priv = ssh_keygen({name => "ssh_host_ecdsa_${::fqdn}", dir => 'ssh/hostkeys', type => 'ecdsa'})
-  $ecdsa_pub  = ssh_keygen({name => "ssh_host_ecdsa_${::fqdn}", dir => 'ssh/hostkeys', type => 'ecdsa', public => 'true'})
+  $ecdsa_pub  = ssh_keygen({name => "ssh_host_ecdsa_${::fqdn}", dir => 'ssh/hostkeys', type => 'ecdsa', public => true})
 
   file { '/etc/ssh/ssh_host_ecdsa_key':
     owner   => 'root',
@@ -68,12 +66,12 @@ class ssh {
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => "ssh-ecdsa $ecdsa_priv host_ecdsa_${::hostname}\n",
+    content => "ssh-ecdsa ${ecdsa_priv} host_ecdsa_${::hostname}\n",
   }
 
   # Generate ED25519 keys reliably
   $ed25519_priv = ssh_keygen({name => "ssh_host_ed25519_${::fqdn}", dir => 'ssh/hostkeys', type => 'ed25519'})
-  $ed25519_pub  = ssh_keygen({name => "ssh_host_ed25519_${::fqdn}", dir => 'ssh/hostkeys', type => 'ed25519', public => 'true'})
+  $ed25519_pub  = ssh_keygen({name => "ssh_host_ed25519_${::fqdn}", dir => 'ssh/hostkeys', type => 'ed25519', public => true})
 
   file { '/etc/ssh/ssh_host_ed25519_key':
     owner   => 'root',
@@ -86,6 +84,6 @@ class ssh {
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => "ssh-ed25519 $ed25519_priv host_ed25519_${::hostname}\n",
+    content => "ssh-ed25519 ${ed25519_priv} host_ed25519_${::hostname}\n",
   }
 }
